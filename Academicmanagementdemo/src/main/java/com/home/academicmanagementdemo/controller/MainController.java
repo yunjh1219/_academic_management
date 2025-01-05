4ffc76e604a8992a9816348f4f511b87ab5687ab
[yunjh1219] <[orenjh1219@gmail.com]>
2025-01-05 23:43:38 +0900

    사이드바 완성, css만 하면 됨

--- Academicmanagementdemo/src/main/java/com/home/academicmanagementdemo/controller/MainController.java
+++ Academicmanagementdemo/src/main/java/com/home/academicmanagementdemo/controller/MainController.java
@@ -7,13 +7,54 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
 
-    @GetMapping("/")
-    public String courseRegInq() {return "pages/student/course-reg-inq";}
 
-    @GetMapping("/course-reg-inq")
+
+    @GetMapping("/")
     public String notice() {return "pages/student/notice";}
 
-    @GetMapping("/tab2")
-    public String courseRegIntq() {return "pages/student/tab2";}
+    //수강
+    @GetMapping("/cour-lnq")
+    public String courlnq() {return "pages/student/courseenrollment/course-enrollment-lnquiry";}
+
+    @GetMapping("/cour-ent")
+    public String courent() {return "pages/student/courseenrollment/course-timetable-management";}
+
+    //출석
+    @GetMapping("/atten-iry")
+    public String attiry() {return "pages/student/attendance/attendance-inquiry";}
+
+    //과제
+    @GetMapping("/ass-iry")
+    public String assent() {return "pages/student/assignment/assignment-inquiry";}
+
+    //성적
+    //금학기 성적 조회
+    @GetMapping("/gra-cur-iry")
+    public String gracur() {return "pages/student/grade/current-semester-grade-inquiry";}
+
+    //기이수 성적 조회
+    @GetMapping("/gra-pre-iry")
+    public String grapre() {return "pages/student/grade/previous-semester-grade-inquiry";}
+
+    //등록
+    //등록금 납부 확인
+    @GetMapping("/regi-loaa")
+    public String regiloaa() {return "pages/student/registration/tuition-payment-confirmation";}
+
+    //등록금 고지서 확인
+    @GetMapping("/regi-ra")
+    public String regira() {return "pages/student/registration/tuition-bill-confirmation";}
+
+    //복학신청
+    @GetMapping("/regi-tbc")
+    public String regitbc() {return "pages/student/registration/readmission-application";}
+
+    //휴학신청
+    @GetMapping("/regi-tpc")
+    public String regitpc() {return "pages/student/registration/leave-of-absence-application";}
+
+    //장학
+    @GetMapping("/schol-sdc")
+    public String scholsdc() {return "pages/student/scholarship/scholarship-details-confirmation";}
 
 }
--- Academicmanagementdemo/src/main/resources/static/js/fragments/sidebar.js
+++ Academicmanagementdemo/src/main/resources/static/js/fragments/sidebar.js
@@ -1,76 +1,28 @@
 $(function() {
-    var tabTitle = $("#tab_title"),
-        tabContent = $("#tab_content"),
-        tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close' role='presentation'>Remove Tab</span></li>",
-        tabCounter = 2;
-
     var tabs = $("#tabs").tabs();
 
-    // 탭을 추가하는 다이얼로그 설정
-    var dialog = $("#dialog").dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            Add: function() {
-                addTab();
-                $(this).dialog("close");
-            },
-            Cancel: function() {
-                $(this).dialog("close");
-            }
-        },
-        close: function() {
-            form[0].reset();
-        }
-    });
-
-    var form = dialog.find("form").on("submit", function(event) {
-        addTab();
-        dialog.dialog("close");
-        event.preventDefault();
-    });
-
-    // 탭을 추가하는 함수
-    function addTab() {
-        var label = tabTitle.val() || "Tab " + tabCounter,
-            id = "tabs-" + tabCounter,
-            li = $(tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label )),
-            tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
-
-        // 새로운 탭을 탭 목록에 추가
-        tabs.find(".ui-tabs-nav").append(li);
-        // 새로운 탭의 내용을 추가
-        tabs.append("<div id='" + id + "'><p>" + tabContentHtml + "</p></div>");
-        tabs.tabs("refresh");
-        tabCounter++;
-    }
-
-    // 메뉴 항목을 클릭할 때 해당 탭 생성 및 활성화
-    $("#menu a").on("click", function(event) {
+    // 메뉴 항목을 클릭할 때 해당 탭 생성 및 활성화 (snb_depth3 a 요소에만 적용)
+    $("#menu .snb_depth3 a").on("click", function(event) {
         event.preventDefault();
         var tabName = $(this).data("tab");  // 클릭한 메뉴 항목의 data-tab 값
-        var tabId = "tabs-" + tabName;  // 탭 ID 설정
+        var tabId = "tabs-" + tabName.replace(/\s+/g, '');  // 공백 제거 후 탭 ID 생성
         var url = $(this).attr("href");  // 링크의 href 값을 가져옴
 
+        // 이미 존재하는 탭인지 확인
         var existingTab = $("#" + tabId);
 
-        // 이미 탭이 존재하면 해당 탭으로 이동
         if (existingTab.length === 0) {
-            // 새로운 탭을 탭 목록에 추가
-            var tabLabel = tabName;
-            var tabContentHtml = "<p>로딩 중...</p>";  // 기본 텍스트 설정
+            // 새로운 탭 추가
+            $("#tabs ul").append("<li><a href='#" + tabId + "'>" + tabName + "</a><span class='ui-icon ui-icon-close' role='presentation'></span></li>");
+            $("#tabs").append("<div id='" + tabId + "'><p>로딩 중...</p></div>");
+            tabs.tabs("refresh");
 
-            // 새로운 탭을 탭 목록에 추가
-            $("#tabs ul").append("<li><a href='#" + tabId + "'>" + tabLabel + "</a><span class='ui-icon ui-icon-close' role='presentation'></span></li>");
-            $("#tabs").append("<div id='" + tabId + "'><p>" + tabContentHtml + "</p></div>");
-            $("#tabs").tabs("refresh");
-
-            // Ajax로 외부 콘텐츠를 로드하여 해당 탭에 삽입
+            // Ajax로 외부 콘텐츠 로드
             $.ajax({
-                url: url, // href 값을 url로 사용
+                url: url,
                 method: 'GET',
                 success: function(data) {
-                    $("#" + tabId).html(data);  // 외부 콘텐츠를 해당 탭에 삽입
+                    $("#" + tabId).html(data);
                 },
                 error: function() {
                     $("#" + tabId).html("<p>콘텐츠를 불러오는 데 실패했습니다.</p>");
@@ -78,25 +30,14 @@ $(function() {
             });
         }
 
-        // 해당 탭을 활성화
-        $("#tabs").tabs("option", "active", $("#tabs").find("a[href='#" + tabId + "']").parent().index());
+        // 해당 탭 활성화
+        tabs.tabs("option", "active", $("#tabs").find("a[href='#" + tabId + "']").parent().index());
     });
 
-
-// 닫기 아이콘 클릭 시 해당 탭 삭제
+    // 닫기 아이콘 클릭 시 탭 삭제
     tabs.on("click", "span.ui-icon-close", function() {
-        $(this).text('');  // 텍스트 제거
         var panelId = $(this).closest("li").remove().attr("aria-controls");
         $("#" + panelId).remove();
         tabs.tabs("refresh");
-    });
-
-    // 백스페이스 키를 누를 때 활성 탭을 삭제
-    tabs.on("keyup", function(event) {
-        if (event.altKey && event.keyCode === $.ui.keyCode.BACKSPACE) {
-            var panelId = tabs.find(".ui-tabs-active").remove().attr("aria-controls");
-            $("#" + panelId).remove();
-            tabs.tabs("refresh");
-        }
     });
 });
